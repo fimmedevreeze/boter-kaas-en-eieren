cddc69a1f4de40df1924f0aa9463fe4a18c4d842
fimmedevreeze <45490744+fimmedevreeze@users.noreply.github.com>
2020-12-17 13:21:19 +0100

    myrandomagent toegev., computer vs zichzelf

--- Speltheorie met python.py
+++ Speltheorie met python.py
@@ -1,3 +1,9 @@
-from bke import start
+import random
+from bke import EvaluationAgent, start
 
-start()
+class MyRandomAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1,500)
+
+my_random_agent = MyRandomAgent()
+start(player_x=my_random_agent, player_o=my_random_agent)
