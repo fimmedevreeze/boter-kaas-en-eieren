97bc7b687827c0f79c27e32657096330c79ce9ab
fimmedevreeze <45490744+fimmedevreeze@users.noreply.github.com>
2020-12-17 18:15:24 +0100

    Mijnspeler toegevoegd

--- Speltheorie met python.py
+++ Speltheorie met python.py
@@ -1,9 +1,18 @@
 import random
-from bke import EvaluationAgent, start
+from bke import *
 
 class MyRandomAgent(EvaluationAgent):
     def evaluate(self, board, my_symbol, opponent_symbol):
         return random.randint(1,500)
 
+class MijnSpeler(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        if can_win(board, opponent_symbol):
+            getal = getal - 50
+        return getal
+ 
+mijn_speler = MijnSpeler()
 my_random_agent = MyRandomAgent()
-start(player_x=my_random_agent, player_o=my_random_agent)
+start(player_x=my_random_agent, player_o=mijn_speler)
+
