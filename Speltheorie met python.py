38af681904ed1ec834df94a447f6261fac4e351f
fimmedevreeze <45490744+fimmedevreeze@users.noreply.github.com>
2021-01-05 20:47:46 +0100

    Can_win en is_winner toegevoegd met waardes
    Hij heeft nu vier van de 5 potjes gewonnen, ééntje gelijk spel (de eerste).

--- Speltheorie met python.py
+++ Speltheorie met python.py
@@ -8,11 +8,17 @@ class MyRandomAgent(EvaluationAgent):
 class MijnSpeler(EvaluationAgent):
     def evaluate(self, board, my_symbol, opponent_symbol):
         getal = 1
-        if can_win(board, opponent_symbol):
-            getal = getal - 50
+        if board[4] == my_symbol:
+            getal += 2
+        if can_win(board, my_symbol):
+            getal += 3
+        if is_winner(board, my_symbol):
+            getal = 10
+        elif can_win(board, opponent_symbol):
+            getal = -10
         return getal
  
 mijn_speler = MijnSpeler()
 my_random_agent = MyRandomAgent()
-start(player_x=my_random_agent, player_o=mijn_speler)
+start(player_o=mijn_speler, player_x=my_random_agent)
 
